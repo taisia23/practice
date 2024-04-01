c388c095586623bcb136a4d72babf174a27ab375
Таїсія Деркач <137027004+taisia23@users.noreply.github.com>
2024-04-01 10:25:34 +0300

    Update 1.java

--- docs/1.java
+++ docs/1.java
@@ -1,9 +1,5 @@
-public class Main {
+class Main {
     public static void main(String[] args) {
-        System.out.println("Кількість аргументів командного рядка: " + args.length);
-        System.out.println("Аргументи командного рядка:");
-        for (int i = 0; i < args.length; i++) {
-            System.out.println((i + 1) + ": " + args[i]);
-        }
+        System.out.println("Hello World!");
     }
 }
